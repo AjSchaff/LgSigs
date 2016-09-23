f5b4fb9dab5bda4d309c374bfcef7c8c8c6b45e0
AjSchaff <aschaff4@gmail.com>
2016-09-23 16:13:29 -0400

    added handlebars

--- js/app.js
+++ js/app.js
@@ -7,13 +7,13 @@ var bodyParser = require('body-parser');
 var session = require('client-sessions');
 var crypto = require('crypto');
 
-var routes = require('./routes/index');
+var routes = require('../routes/index');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
